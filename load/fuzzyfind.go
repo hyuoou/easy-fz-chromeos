19da3b41d4b864f71aedcbaedab6bcddbdc62ac8
hyuoou <63180765+hyuoou@users.noreply.github.com>
2022-03-04 20:23:56 +0900

    Update load/fuzzyfind.go

--- load/fuzzyfind.go
+++ load/fuzzyfind.go
@@ -13,7 +13,7 @@ func ShowFuzzyfinder(ChromeosDeviceList []ChromeOS) (idx int, err error) {
 			return ChromeosDeviceList[i].Model
 		},
 		fuzzyfinder.WithPreviewWindow(func(i, width, height int) string {
-			return fmt.Sprintf("Channel       : %s\nFile          : %s\nFilesize      : %vB\nHwidmatch     : %s\nManufacturer  : %s\nMd5           : %s\nModel         : %s\nName          : %s\nSha1          : %s\nUrl           : %s\nVersion       : %s\nZipfilesize   : %vB\nChromeVersion : %s\n",
+			return fmt.Sprintf("Channel       : %s\nFile          : %s\nFilesize      : %vB\nHwidmatch     : %s\nManufacturer  : %s\nMd5           : %s\nModel         : %s\nName          : %s\nSha1          : %s\nVersion       : %s\nZipfilesize   : %vB\nChromeVersion : %s\n",
 				ChromeosDeviceList[i].Channel,
 				ChromeosDeviceList[i].File,
 				ChromeosDeviceList[i].Filesize,
@@ -23,7 +23,6 @@ func ShowFuzzyfinder(ChromeosDeviceList []ChromeOS) (idx int, err error) {
 				ChromeosDeviceList[i].Model,
 				ChromeosDeviceList[i].Name,
 				ChromeosDeviceList[i].Sha1,
-				ChromeosDeviceList[i].Url,
 				ChromeosDeviceList[i].Version,
 				ChromeosDeviceList[i].Zipfilesize,
 				ChromeosDeviceList[i].ChromeVersion)
