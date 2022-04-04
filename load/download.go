9211d1fb2a0889972db44cc2cc95b348b1719628
hyuoou <63180765+hyuoou@users.noreply.github.com>
2022-04-04 23:07:26 +0900

    Use http.Get

--- load/download.go
+++ load/download.go
@@ -2,20 +2,31 @@ package load
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
-	"os/exec"
 	"os/signal"
+	"strings"
 )
 
 func Download(url string, fileName string, downloadPath string) {
+	if strings.Contains(downloadPath, "$HOME") {
+		strings.Replace(downloadPath, "$HOME", os.Getenv("HOME"), 1)
+	}
 	go func() {
 		fmt.Println("Start downloading ChromeOS for the selected model name. Please wait")
-		err := exec.Command("wget", "-P", downloadPath, url).Run()
+		resp, err := http.Get(url)
 		if err != nil {
 			log.Fatalln(err)
 		}
-		fmt.Println("Done")
+		defer resp.Body.Close()
+		path, err := os.Create(downloadPath + "/" + fileName + ".zip")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer path.Close()
+		io.Copy(path, resp.Body)
 		os.Exit(0)
 	}()
 
@@ -23,6 +34,9 @@ func Download(url string, fileName string, downloadPath string) {
 	signal.Notify(quit, os.Interrupt)
 	<-quit
 	filePath := downloadPath + "/" + fileName + ".zip"
-	os.Remove(filePath)
+	err := os.Remove(filePath)
+	if err != nil {
+		fmt.Println(err)
+	}
 	os.Exit(1)
 }
